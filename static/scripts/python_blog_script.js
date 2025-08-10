deb9e6beb2615c7abd1bc053ea037e97ec486e75
r021n <rozinabdul25@gmail.com>
2025-08-10 21:27:23 +0700

    - adding new chapter about output, comments, and variable

---
+++ static/scripts/python_blog_script.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const navbar = document.getElementById("floating-navbar");
+  const h1Text = document.querySelector("h1").innerText;
+  document.querySelector(".nav-title").innerText = h1Text;
+
+  let lastScrollY = window.scrollY;
+  let navbarHeight = navbar.offsetHeight;
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > lastScrollY) {
+      // Scrolling down
+      navbar.style.transform = "translateY(100%)";
+      navbar.style.opacity = "0";
+      document.body.style.setProperty("--whatsapp-bottom", "20px");
+    } else {
+      // Scrolling up
+      navbar.style.transform = "translateY(0)";
+      navbar.style.opacity = "1";
+      navbarHeight = navbar.offsetHeight;
+      document.body.style.setProperty(
+        "--whatsapp-bottom",
+        `${navbarHeight + 20}px`
+      );
+    }
+    lastScrollY = window.scrollY;
+  });
+
+  // Initial check
+  if (window.scrollY === 0) {
+    navbarHeight = navbar.offsetHeight;
+    document.body.style.setProperty(
+      "--whatsapp-bottom",
+      `${navbarHeight + 20}px`
+    );
+    navbar.style.transform = "translateY(0)";
+    navbar.style.opacity = "1";
+  }
+
+  // Initialize syntax highlighting
+  hljs.highlightAll();
+});
